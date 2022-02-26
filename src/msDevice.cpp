4d94db77d6c4f09fa6c125b39e4e03e733bcd530
Warren Watson <wwatson4506@gmail.com>
2022-02-26 10:57:48 -0800

    Update msDevice.cpp
    Changed MSDevice.h to msDevice.h

--- src/msDevice.cpp
+++ src/msDevice.cpp
@@ -26,7 +26,7 @@
 
 #include <Arduino.h>
 #include "USBHost_t36.h"  // Read this header first for key info
-#include "MSDevice.h"
+#include "msDevice.h"
 
 #define print   USBHost::print_
 #define println USBHost::println_
